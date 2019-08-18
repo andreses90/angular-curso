11a91e15ba9105feb7f37650f1a2e671116f6f0d
andreses90 <escaresh@gmail.com>
2019-08-18 01:21:46 -0400

    hasta generacion de servicio HeroeService

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService ,Heroe } from '../../servicios/heroes.service'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit() {
+
+    this.heroes = this.heroesService.getHeroes();
+    console.log(this.heroes);
+
+  }
+
+}
